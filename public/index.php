4c71364fb1b9ac467489b2d6bdab4970c8a8f53c
Tomokazu Tantaka <t.tantaka@eukarya.io>
2024-12-01 13:21:40 +0900

    update

--- public/index.php
+++ public/index.php
@@ -10,16 +10,49 @@ ini_set('max_execution_time', 300);
 use Slim\App;
 use UMA\DIC\Container;
 use FpvJp\DI;
+use Doctrine\Common\Cache\Psr6\DoctrineProvider;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\ORMSetup;
+use Doctrine\DBAL\DriverManager;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use UMA\DIC\Container;
+use FpvJp\Logger\EchoSQLLogger;
+// use Cloudinary\Configuration\Configuration;
 
 /** @var Container $cnt */
-$cnt = require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-$cnt->register(new DI\Doctrine());
-$cnt->register(new DI\CloudinaryAdmin());
-$cnt->register(new DI\Wasabi());
-$cnt->register(new DI\Mailer());
-$cnt->register(new DI\Slim());
+$container = new Container(require __DIR__ . '/settings.php');
+
+$container->set(EntityManager::class, static function (Container $c): EntityManager {
+    /** @var array $settings */
+    $settings = $c->get('settings');
+
+    $config = ORMSetup::createAttributeMetadataConfiguration(
+        $settings['doctrine']['metadata_dirs'],
+        $settings['doctrine']['dev_mode'],
+        null,
+        $settings['doctrine']['dev_mode'] ?
+        new ArrayAdapter() :
+        new FilesystemAdapter(directory: $settings['doctrine']['cache_dir']),
+        true
+    );
+    if ($settings['doctrine']['dev_mode']) {
+        $config->setSQLLogger(new EchoSQLLogger());
+    }
+    $connection = DriverManager::getConnection($settings['doctrine']['connection'], $config);
+    return new EntityManager($connection, $config);
+});
+
+$container->register(new DI\Doctrine());
+$container->register(new DI\CloudinaryAdmin());
+$container->register(new DI\Wasabi());
+$container->register(new DI\Mailer());
+$container->register(new DI\Slim());
 
 /** @var App $app */
-$app = $cnt->get(App::class);
+$app = $container->get(App::class);
 $app->run();
