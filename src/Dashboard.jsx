603774f7a6303cc66979cb421f6dd783487b4a0d
Tomokazu Tantaka <t.tantaka@eukarya.io>
2024-12-11 10:45:25 +0900

    update

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,26 +1,21 @@
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
 
-import { DesktopNavigationMenu, DesktopNavigationUserMenu, MobileNavigationMenu, MobileNavigationUserMenu, Button, IndigoButton, IconButton } from './DashboardMenu'
+import { DesktopNavigationMenu, DesktopNavigationUserMenu, MobileNavigationMenu, MobileNavigationUserMenu, Button, IndigoButton, IconButton, MobileIconButton } from './DashboardMenu'
 import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
 import { Route, Routes, useLocation, Link } from 'react-router-dom'
 import { useEffect } from 'react'
 
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { KeyIcon } from '@heroicons/react/20/solid'
+import { Loading, Error } from './Components'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+import { Users } from './Users'
+import { Users2 } from './Users2'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'Dashboard', href: '/', current: true },
+  { name: 'Users', href: '/users', current: false },
+  { name: 'Users2', href: '/users2', current: false },
 ]
 
 const userNavigation = [
@@ -29,28 +24,42 @@ const userNavigation = [
   { name: 'Sign out', href: '#' },
 ]
 
+const ProtectedUsers = withAuthenticationRequired(Users)
+const ProtectedUsers2 = withAuthenticationRequired(Users2)
+
 export default function Dashboard() {
-  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0()
+  const { isLoading, error, isAuthenticated, user, loginWithRedirect, logout } = useAuth0()
   const { pathname } = useLocation()
 
-  useEffect(() => {
-    console.log(user)
-  }, [user]);
+  // useEffect(() => {
+  //   console.log(user)
+  // }, [user]);
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <>
+      {error && <Error message={error.message} />}
       <div className="min-h-full">
         <Disclosure as="nav" className="bg-gray-800">
-          <DesktopNavigation navigation={navigation} userNavigation={userNavigation} isAuthenticated={isAuthenticated} loginWithRedirect={loginWithRedirect} logout={logout} />
-          <MobileNavigation navigation={navigation} userNavigation={userNavigation} />
+          <DesktopNavigation user={user} pathname={pathname} navigation={navigation} userNavigation={userNavigation} isAuthenticated={isAuthenticated} loginWithRedirect={loginWithRedirect} logout={logout} />
+          <MobileNavigation user={user} navigation={navigation} userNavigation={userNavigation} />
         </Disclosure>
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">{pathname}</h1>
           </div>
         </header>
         <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <Routes>
+              <Route path='/' />
+              <Route path='/users' element={<ProtectedUsers />} />
+              <Route path='/users2' element={<ProtectedUsers2 />} />
+            </Routes>
+          </div>
         </main>
       </div>
     </>
@@ -58,7 +67,7 @@ export default function Dashboard() {
 }
 
 /* eslint-disable react/prop-types */
-function DesktopNavigation({ navigation, userNavigation, isAuthenticated, loginWithRedirect, logout }) {
+function DesktopNavigation({ user, pathname, navigation, userNavigation, isAuthenticated, loginWithRedirect, logout }) {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex h-16 items-center justify-between">
@@ -66,7 +75,7 @@ function DesktopNavigation({ navigation, userNavigation, isAuthenticated, loginW
           <div className="shrink-0">
             <img alt="Your Company" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" className="size-8" />
           </div>
-          <DesktopNavigationMenu navigation={navigation} />
+          <DesktopNavigationMenu pathname={pathname} navigation={navigation} />
         </div>
         <div className="hidden md:block">
           {isAuthenticated ? (<>
@@ -103,27 +112,24 @@ function DesktopNavigation({ navigation, userNavigation, isAuthenticated, loginW
 }
 
 /* eslint-disable react/prop-types */
-function MobileNavigation({ navigation, userNavigation }) {
+function MobileNavigation({ user, navigation, userNavigation }) {
   return (
     <DisclosurePanel className="md:hidden">
       <MobileNavigationMenu navigation={navigation} />
       <div className="border-t border-gray-700 pb-3 pt-4">
         <div className="flex items-center px-5">
           <div className="shrink-0">
-            <img alt="" src={user.imageUrl} className="size-10 rounded-full" />
+            <img alt="" src={user.picture} className="size-10 rounded-full" />
           </div>
           <div className="ml-3">
             <div className="text-base/5 font-medium text-white">{user.name}</div>
             <div className="text-sm font-medium text-gray-400">{user.email}</div>
           </div>
-          <button
-            type="button"
-            className="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          >
+          <MobileIconButton>
             <span className="absolute -inset-1.5" />
             <span className="sr-only">View notifications</span>
             <BellIcon aria-hidden="true" className="size-6" />
-          </button>
+          </MobileIconButton>
         </div>
         <MobileNavigationUserMenu userNavigation={userNavigation} />
       </div>
--- src/DashboardMenu.jsx
+++ src/DashboardMenu.jsx
@@ -1,4 +1,5 @@
 import { DisclosureButton, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Route, Routes, useLocation, Link } from 'react-router-dom'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -32,22 +33,29 @@ export function IconButton({ children, ...props }) {
 }
 
 /* eslint-disable react/prop-types */
-export function DesktopNavigationMenu({ navigation }) {
+export function MobileIconButton({ children, ...props }) {
+  return (
+    <button type="button" className="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" {...props}>
+      {children}
+    </button>
+  )
+}
+
+/* eslint-disable react/prop-types */
+export function DesktopNavigationMenu({ pathname, navigation }) {
   return (
     <div className="hidden md:block">
       <div className="ml-10 flex items-baseline space-x-4">
         {navigation.map((item) => (
-          <a
+          <Link
             key={item.name}
-            href={item.href}
-            aria-current={item.current ? 'page' : undefined}
+            to={item.href}
+            aria-current={pathname === item.href ? 'page' : undefined}
             className={classNames(
-              item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-              'rounded-md px-3 py-2 text-sm font-medium',
-            )}
-          >
+              pathname === item.href ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium',
+            )}>
             {item.name}
-          </a>
+          </Link>
         ))}
       </div>
     </div>
@@ -62,7 +70,7 @@ export function DesktopNavigationUserMenu({ user, userNavigation, logout }) {
         <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
           <span className="absolute -inset-1.5" />
           <span className="sr-only">Open user menu</span>
-          <img alt="" src={user.imageUrl} className="size-8 rounded-full" />
+          <img alt="" src={user.picture} className="size-8 rounded-full" />
         </MenuButton>
       </div>
       <MenuItems transition className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
