32fbb8152413dc88d1fd99d080c9869fc1992ab0
Tomokazu Tantaka <t.tantaka@eukarya.io>
2024-12-31 07:33:09 +0900

    aa

--- src/example-webrtc/WebRTC.jsx
+++ src/example-webrtc/WebRTC.jsx
@@ -7,13 +7,13 @@ import { initialValue, rtc_configuration, default_constraints, websocketServerUR
 export function WebRTCHeader() {
   return (
     <div className='mt-6 flex max-w-md gap-x-4'>
-      <label htmlFor='email-address' className='sr-only'>
-        Email address
-      </label>
-      <input id='email-address' name='email' type='email' required placeholder='Enter your email' autoComplete='email' 
-      className='min-w-0 flex-auto rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6' />
+      <input name='email' type='email' required placeholder="The other person's ID" autoComplete='email' 
+      className='block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6' />
       <button type='submit' className='flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'>
-        Subscribe
+      {/* <PhoneArrowUpRightIcon aria-hidden='true' className='-ml-0.5 mr-1.5 size-5' /> */}
+      {/* <PhoneXMarkIcon aria-hidden='true' className='-ml-0.5 mr-1.5 size-5' /> */}
+      connect
+      {/* {state['peer-connect-button']} */}
       </button>
     </div>
   )
@@ -248,11 +248,11 @@ export default function ExamleWebRTC() {
   return (
     <div>
       <div>
-        Currently waiting PeerID is <PhoneArrowDownLeftIcon aria-hidden='true' className='-ml-0.5 mr-1.5 size-5' />
+        Currently waiting PeerID is
         <b id='peer-id'>{state['peer-id']}</b>
       </div>
 
-      <FormComponents.Input type='text' label={'Enter peer ID'} htmlFor={'peer-connect'} onChange={inputChange} autoComplete={'peer-connect'} />
+      {/* <FormComponents.Input type='text' label={'Enter peer ID'} htmlFor={'peer-connect'} onChange={inputChange} autoComplete={'peer-connect'} />
 
       <Components.IndigoButton onClick={onConnectClicked}>
         <PhoneArrowUpRightIcon aria-hidden='true' className='-ml-0.5 mr-1.5 size-5' />
@@ -264,7 +264,7 @@ export default function ExamleWebRTC() {
         {state['peer-connect-button']}
       </Components.ButtonDeactivate>
 
-      <FormComponents.CheckboxInput label={'Remote offerer'} htmlFor={'remote-offerer'} onChange={inputChange} description={'Remote offerer'} />
+      <FormComponents.CheckboxInput label={'Remote offerer'} htmlFor={'remote-offerer'} onChange={inputChange} description={'Remote offerer'} /> */}
 
       <div className='flex h-[60vh] w-full items-center justify-center'>
         <video ref={receive_video} autoPlay playsInline className='hidden max-h-full max-w-full'>
